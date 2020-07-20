50ca4fb34cd4ebea2fd29e0d5db73d5e573174ce
Jaroslav Zivny <jarda668@gmail.com>
2020-07-20 17:28:43 +0200

    Desable checkov

--- components/importer/lambda.tf
+++ components/importer/lambda.tf
@@ -1,13 +1,10 @@
 resource "aws_lambda_function" "importer" {
+  # checkov:skip=CKV_AWS_50:skipping
   filename         = "resources/hello-world.zip"
   function_name    = var.name_prefix
   role             = aws_iam_role.importer.arn
   handler          = "src/handlers/import.lambda_handler"
   source_code_hash = filebase64sha256("resources/hello-world.zip")
-
-  tracing_config {
-    mode = "Active"
-  }
 
   runtime = "python3.8"
 
