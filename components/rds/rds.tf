ac6c26e8740202604d2356aedece7a1c145ee828
Jaroslav Zivny <jarda668@gmail.com>
2020-07-20 16:48:24 +0200

    Update rdsd

--- components/rds/rds.tf
+++ components/rds/rds.tf
@@ -4,15 +4,16 @@ resource "random_string" "password" {
 }
 
 resource "aws_db_instance" "rds" {
-  allocated_storage = var.allocated_storage
-  storage_type      = "gp2"
-  engine            = "postgres"
-  engine_version    = "11.6"
-  instance_class    = var.instance_class
-  name              = var.name_prefix
-  username          = "postgres"
-  password          = random_string.password.result # Will be changed manually
-  kms_key_id        = aws_kms_key.rds.key_id
-  storage_encrypted = true
-  tags              = var.tags
+  allocated_storage         = var.allocated_storage
+  storage_type              = "gp2"
+  engine                    = "postgres"
+  engine_version            = "11.6"
+  instance_class            = var.instance_class
+  name                      = var.name_prefix
+  username                  = "postgres"
+  password                  = random_string.password.result # Will be changed manually
+  kms_key_id                = aws_kms_key.rds.arn
+  final_snapshot_identifier = var.name_prefix
+  storage_encrypted         = true
+  tags                      = var.tags
 }
