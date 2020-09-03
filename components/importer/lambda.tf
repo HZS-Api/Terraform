3acb3db2cbbeea106171f8b626b04a51eda653b5
Jaroslav Zivny <jarda668@gmail.com>
2020-09-03 17:51:53 +0200

    Update lambda

--- components/importer/lambda.tf
+++ components/importer/lambda.tf
@@ -3,10 +3,18 @@ resource "aws_lambda_function" "importer" {
   filename         = "resources/hello-world.zip"
   function_name    = var.name_prefix
   role             = aws_iam_role.importer.arn
-  handler          = "src/handlers/import.lambda_handler"
+  handler          = "src/import.lambda_handler"
   source_code_hash = filebase64sha256("resources/hello-world.zip")
+  layers = [aws_lambda_layer_version.dependencies.arn]
 
   runtime = "python3.8"
 
   tags = var.tags
+}
+
+resource "aws_lambda_layer_version" "dependencies" {
+  filename   = "resources/hello-world.zip"
+  layer_name = "${var.name_prefix}-dependencies"
+
+  compatible_runtimes = ["python3.8"]
 }
